b644624faa91f06c4f4a2babbdda56100d7d29fb
liuyongming <liuyongming@shopex.cn>
2017-10-26 02:22:50 +0800

    test

--- application/controllers/Wxnotify.php
+++ application/controllers/Wxnotify.php
@@ -17,7 +17,6 @@ class Wxnotify extends PublicController
     {
         log_message("info", "weixin response event, get:" . var_export($_GET, true) . ", post:" . var_export($_POST, true));
         $this->load->model("UserModel", "user", true);
-        $this->load->model("SceneModel", "scene", true);
         $options = array(
             "appid"          => QIUKONG_APPID,
             "appsecret"      => QIUKONG_SECRET,
@@ -51,30 +50,6 @@ class Wxnotify extends PublicController
                             "Description" => "",
                             "PicUrl"      => "https://mmbiz.qlogo.cn/mmbiz_jpg/e9HZsSmNVsqxqyENMMNcIyvIC90B63xIp5ghpla3q7AG4bALBGXMjGFJ7Nib54ccELoCLicibhcXmOURFv8PLLtKQ/0?wx_fmt=jpeg",
                             "Url"         => "http://mp.weixin.qq.com/s/bAnGD8_a8rp5Xh8oqLZn5A"
-                        ),
-                        array(
-                            "Title"       => "如何绑定银行卡? 提现?",
-                            "Description" => "",
-                            "PicUrl"      => "https://mmbiz.qlogo.cn/mmbiz_png/e9HZsSmNVsqxqyENMMNcIyvIC90B63xIzDOrJibNZRuxSloqMzUwZZgDqyTUeRuiaBI2H1bvQ0Z9ibcWNe7U15AkA/0?wx_fmt=png",
-                            "Url"         => "http://mp.weixin.qq.com/s/cdWl-1XHKdv08LLl4NVnrQ"
-                        ),
-                        array(
-                            "Title"       => "我要印刷收款码怎么弄?",
-                            "Description" => "",
-                            "PicUrl"      => "https://mmbiz.qlogo.cn/mmbiz_png/e9HZsSmNVsqxqyENMMNcIyvIC90B63xInDek45pIKm59KWUFibTosqKVYiaC9yhkJgiayibUN1ia57ZSsTUHN9qGjow/0?wx_fmt=png",
-                            "Url"         => "http://mp.weixin.qq.com/s/1x7E4ACF5UChKYbMzv1baw"
-                        ),
-                        array(
-                            "Title"       => "如何查询及导出对账单",
-                            "Description" => "",
-                            "PicUrl"      => "https://mmbiz.qlogo.cn/mmbiz_png/e9HZsSmNVsqxqyENMMNcIyvIC90B63xIiaUOebqicQaTg1eFoao8FYGBAWoraPrsKdsaGQmGOUcCjq4iac7qotyIg/0?wx_fmt=png",
-                            "Url"         => "http://mp.weixin.qq.com/s/q4SEOttmObh3bUct7pfQHg"
-                        ),
-                        array(
-                            "Title"       => "客服联系方式",
-                            "Description" => "",
-                            "PicUrl"      => "https://mmbiz.qlogo.cn/mmbiz_png/e9HZsSmNVsqxqyENMMNcIyvIC90B63xI6g0nubIaEzRsSMGN9liaM0RRzbGAQF4jhLQgw4icoLQbicznhboPmpegg/0?wx_fmt=png",
-                            "Url"         => "http://mp.weixin.qq.com/s/n1y9yIyIpk3GegbnvYOmFw"
                         )
                     ),
                     "FuncFlag"     => 0
